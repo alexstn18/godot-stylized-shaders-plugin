c4b932d8e3f20e2e830153d058e38770796dbc83
alex s. <116825414+alexstn18@users.noreply.github.com>
2025-09-11 16:24:38 +0200

    [Add] Fix for .dev build name; [Fix] Error on project load

---
+++ rename.py
@@ -0,0 +1,15 @@
+import os
+
+folder = "demo/addons/GodotStylizedShadersPlugin"
+
+for filename in os.listdir(folder):
+    if "dev" in filename:
+        new_name = filename.replace(".dev", "")
+        old_path = os.path.join(folder, filename)
+        new_path = os.path.join(folder, new_name)
+
+        if os.path.exists(new_path):
+            print(f'Skipping "{filename}" → "{new_name}" (already exists)')
+        else:
+            os.rename(old_path, new_path)
+            print(f'Renamed: "{filename}" → "{new_name}"')
